a395a6ab009c121062dfa8d4e9bed37b862540b7
elisasaltori <elisa.trujillo@usp.br>
2018-06-09 09:42:13 -0300

    added matplotlib colormap inferno

--- main.py
+++ main.py
@@ -63,29 +63,22 @@ def median_filter(img_in, n):
 
     return img_out
     
-def colorize_image(img_in):
 
+def baggage_colorize(img_in):
+    
+    #image dimensions
     x,y = np.shape(img_in)
+
+    #tresholds for color mapping using the HSV system
+    #mapping lighter intensity to orange, intermediate to green and high intensity to blue
     low_tresh = 130
     up_tresh = 200
-    """
-    #rgb
-    img_out = np.zeros((x,y,3), dtype=np.uint8)
-    """
+
     rev = 255-img_in
     img_out = np.zeros((x,y,3))
-    #img_out[:,:,0]=rev/255.0*(2/3)
-    #mapping lighter intensity to orange, intermediate to green and high intensity to blue
-    
+
+
     img_out[:,:,0]=rev
-    """
-    #orange
-    img_out[img_out<low_tresh]=47.0/360.0
-    #blue
-    img_out[img_out>=up_tresh]=236.0/360.0 
-    #green
-    img_out[img_out>=low_tresh]=100.0/360.0
-    """
 
     #defining hue
     #orange
@@ -100,21 +93,80 @@ def colorize_image(img_in):
    
 
     img_out[:,:,1]=rev/255.0*2 #saturation
-    img_out[:,:,2]=img_in/255.0 #value: map according to "faixa"
-    #img_out[:,:,2]=value
-
-    mask = rev
-   # mask[mask==0]=0
-    #mask[mask>0]=1
-    #img_out[:,:,1] = np.multiply(img_out[:,:,1],mask)
-
+    img_out[:,:,2]=img_in/255.0 #value
 
     img_out = mat.colors.hsv_to_rgb(img_out)
 
+    return img_out
 
+def hot_colormap(img_in):
+    
+    #image dimensions
+    x,y = np.shape(img_in)
+    img_in = 255-img_in
+    img_out = np.zeros((x,y,3))
+
+    #rgb
+    #red
+    aux = np.zeros((x,y))
+    aux = img_in/255.0
+    aux[aux>=0.35]=1
+    aux[aux<0.35]=(aux[aux<0.35]+0.01)/(0.35+0.01)
+  
+
+    img_out[:,:,0] = aux
+
+    #green
+    aux = np.zeros((x,y))
+    aux = img_in/255.0
+    aux[aux>=0.74]=1
+    aux[aux<0.74]=(aux[aux<0.74]-0.35)/(0.74-0.35)
+    img_out[:,:,1] = aux
+  
+    #blue
+    aux = np.zeros((x,y))
+    aux = img_in/255.0
+    aux=(aux-0.74)/(1-0.74)
+    img_out[:,:,2] = aux
+    #-0.01|0.35
+    #0.35|0.74
+    #0.74|1
 
     return img_out
 
+def inferno_colormap(img_in):
+
+    x,y = np.shape(img_in)
+    img_out = np.zeros((x,y,3))
+    img_out = plt.cm.inferno(img_in)
+
+    return img_out
+
+
+def colorize_image(img_in, method):
+
+    if (method==1):
+        return baggage_colorize(img_in)
+
+    if (method==2):
+        return hot_colormap(img_in)
+
+    if(method==3):
+        return inferno_colormap(img_in)
+
+def equalize(img_in, method):
+
+    if (method==1):
+        return normalize(img_in)
+    if (method==2):
+        return histrogram_equalize(img_in)
+
+def normalize(img_in):
+
+    return (255*(img_in-np.min(img_in))/(np.max(img_in)-np.min(img_in)))
+
+def histrogram_equalize(img_in):
+    pass
 
 
 def main():
@@ -123,7 +175,7 @@ def main():
     #img_name = str(input()).rstrip()
     img_name = "B0009_0001.png"
     #img_name = "00000061_002.png"
-    img_name = "B0023_0001.png"
+    #img_name = "B0023_0001.png"
     #reads original image
     img = imageio.imread(img_name)
     plt.subplot(131)
@@ -136,11 +188,12 @@ def main():
     """
 
     img_out = median_filter(img, 3)
-    img_out = 255*(img_out-np.min(img_out))/(np.max(img_out)-np.min(img_out))
+    img_out = equalize(img_out, 1)
     plt.subplot(132)
     plt.imshow(img_out, cmap="gray")
 
-    img_out = colorize_image(img)
+    method=3
+    img_out = colorize_image(img, method)
     plt.subplot(133)
     plt.imshow(img_out)
 
