1592fa190aeaba47d0fef67fbd0ac8a6795d011a
kenchen12 <kenchen12@gmail.com>
2018-06-11 11:14:10 -0300

    now execute all different combination of tests

--- test.sh
+++ test.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-N=$(ls $2| wc -l)
-FILES=`ls $2`
-for i in `seq 1 $N`
-do
-    FILE=$(echo $FILES| cut -d ' ' -f $i)
-    FULL="$2$FILE"
-    python3 main.py "$1" "$FULL" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}"
-done
-
-
-
+if [ $# -eq 6 ]
+then
+    FILES=`ls $1`
+    N=$(ls $1| wc -l)
+    for i in `seq 1 $N`
+    do
+        FILE=$(echo $FILES| cut -d ' ' -f $i)
+        FULL="$1$FILE"
+        for j in `seq 1 3`
+        do
+            for k in `seq 1 2`
+            do
+                for l in `seq 1 3`
+                do
+                NAME="$j$k$l"
+                python3 main.py "$NAME" "$FULL" "$j" "$2" "$3" "$k" "$4" "$5" "$6" "$l"
+                done
+            done
+        done
+    done
+else
+    echo "usage: (path) (smooth filter size) (smoot sigma) (shap filter size) (sharp sigma) (equalization method)"
+fi
